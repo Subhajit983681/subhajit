23005fea6dc053377e9d8762c5633cb08a8566e1
Subhajit983681 <subhajit9836814898@gmail.com>
2024-11-30 00:38:27 +0530

    add new

--- blocks/counterheader/counterheader.js
+++ blocks/counterheader/counterheader.js
@@ -1,34 +1,27 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
+import { createOptimizedPicture } from "../../scripts/aem.js";
 
 export default function decorate(block) {
-  const topDiv = document.createElement('div');
-  topDiv.classList.add('counter-main-section');
+  const topDiv = document.createElement("div");
+  topDiv.classList.add("counter-main-section");
 
   [...block.children].forEach((row) => {
-    const innerDiv = document.createElement('div');
-    innerDiv.classList.add('counter-inner-part');
+    const innerDiv = document.createElement("div");
+    innerDiv.classList.add("counter-inner-part");
     while (row.firstElementChild) {
       innerDiv.append(row.firstElementChild);
     }
 
-    console.log("innerDiv", innerDiv);
-
-    [...innerDiv.children].forEach((div) => {
-        // div.className = 'counter-data';
-        if (div.firstElementChild) {
-          div.className = 'counter-heading';
-        } else {
-          div.className = 'counter-sub-heading';
-        }
+    [...innerDiv.children].forEach((div, index) => {
+      if (index === 0 && div.firstElementChild) {
+        div.className = "counter-heading";
+      } else {
+        div.className = "counter-sub-heading";
+      }
     });
 
     topDiv.append(innerDiv);
   });
 
-  // ul.querySelectorAll('picture > img').forEach((img) => {
-  //   img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]));
-  // });
-
-  block.textContent = '';
+  block.textContent = "";
   block.append(topDiv);
 }
